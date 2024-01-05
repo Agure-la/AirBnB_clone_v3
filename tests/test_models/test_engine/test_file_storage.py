156d81b4ec91b9b8a9de898b46093b5157332cf1
Agure-la <lameckagure@gmail.com>
2024-01-05 01:44:33 -0800

    added test to a method that count the number of objects in storage

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -124,3 +124,16 @@ class TestFileStorage(unittest.TestCase):
         new_user = User()
         new_user.save()
         self.assertIs(storage.get("User", new_user.id), new_user)
+
+     @unittest.skipIf(os.getenv('HBNB_TYPE_STORAGE') == 'db',
+                     "not testing file storage")
+    def test_count(self):
+        storage = FileStorage()
+        initial_length = len(storage.all())
+        self.assertEqual(storage.count(), initial_length)
+        state_len = len(storage.all("State"))
+        self.assertEqual(storage.count("State"), state_len)
+        new_state = State()
+        new_state.save()
+        self.assertEqual(storage.count(), initial_length + 1)
+        self.assertEqual(storage.count("State"), state_len + 1)
