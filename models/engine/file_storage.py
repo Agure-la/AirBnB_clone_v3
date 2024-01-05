694fb82bdb59f8f06f25c9408adb1d1b3937bf19
Agure-la <lameckagure@gmail.com>
2024-01-05 01:42:01 -0800

    added a method that count the number of objects in storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -69,6 +69,16 @@ class FileStorage:
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
 
+    def get(self, cls, id):
+        """Retrieve an object"""
+        if cls is not None and type(cls) is str and id is not None and\
+           type(id) is str and cls in classes:
+            key = cls + '.' + id
+            obj = self.__objects.get(key, None)
+            return obj
+        else:
+            return None
+
     def count(self, cls=None):
         """Count number of objects in storage"""
         total = 0
