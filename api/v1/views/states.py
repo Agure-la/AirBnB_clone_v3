4927c77fdbb47ad6cc4c45e3795c2df8db27eeee
Agure-la <lameckagure@gmail.com>
2024-01-07 07:41:11 -0800

    added a method to retrieve state by id

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -18,3 +18,13 @@ def get_states():
     for state in all_states:
         list_states.append(state.to_dict())
     return jsonify(list_states)
+
+@app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
+@swag_from('documentation/state/get_id_state.yml', methods=['get'])
+def get_state(state_id):
+    """ Retrieves a specific State """
+    state = storage.get(State, state_id)
+    if not state:
+        abort(404)
+
+    return jsonify(state.to_dict())
