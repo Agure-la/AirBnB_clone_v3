44ac44ee1b7afad5de6dc9ea1d14f6e8942c87f7
Agure-la <lameckagure@gmail.com>
2024-01-07 07:33:31 -0800

    added a function to list all states

---
+++ api/v1/views/states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" objects that handle all default RestFul API actions for States """
+from models.state import State
+from models import storage
+from api.v1.views import app_views
+from flask import abort, jsonify, make_response, request
+from flasgger.utils import swag_from
+
+
+@app_views.route('/states', methods=['GET'], strict_slashes=False)
+@swag_from('documentation/state/get_state.yml', methods=['GET'])
+def get_states():
+    """
+    Retrieves the list of all State objects
+    """
+    all_states = storage.all(State).values()
+    list_states = []
+    for state in all_states:
+        list_states.append(state.to_dict())
+    return jsonify(list_states)
