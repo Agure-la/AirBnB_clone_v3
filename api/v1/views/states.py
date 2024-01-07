0fd81819d1cc88263acbb774d81f09787a61bd91
Agure-la <lameckagure@gmail.com>
2024-01-07 07:43:31 -0800

    added a function to delete a state

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -28,3 +28,22 @@ def get_state(state_id):
         abort(404)
 
     return jsonify(state.to_dict())
+
+@app_views.route('/states/<state_id>', methods=['DELETE'],
+                 strict_slashes=False)
+@swag_from('documentation/state/delete_state.yml', methods=['DELETE'])
+def delete_state(state_id):
+    """
+    Deletes a State Object
+    """
+
+    state = storage.get(State, state_id)
+
+    if not state:
+        abort(404)
+
+    storage.delete(state)
+    storage.save()
+
+    return make_response(jsonify({}), 200)
+
