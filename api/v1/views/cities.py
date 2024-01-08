4fe3a2627cc3b220083ab47461864fd1c1430b6d
Agure-la <lameckagure@gmail.com>
2024-01-08 09:02:24 -0800

    added a function that Retrieves the list of all City objects of a State

---
+++ api/v1/views/cities.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" objects that handles all default RestFul API actions for cities """
+from models.city import City
+from models.state import State
+from models import storage
+from api.v1.views import app_views
+from flask import abort, jsonify, make_response, request
+from flasgger.utils import swag_from
+
+@app_views.route('/states/<string:state_id>/cities', methods=['GET'],
+                 strict_slashes=False)
+def get_cities(state_id):
+    """get city information for all cities in a specified state"""
+    state = storage.get("State", state_id)
+    if state is None:
+        abort(404)
+    cities = []
+    for city in state.cities:
+        cities.append(city.to_dict())
+    return jsonify(cities)
