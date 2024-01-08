556a587d96488b0f73348fcf7514b37923568cf3
Agure-la <lameckagure@gmail.com>
2024-01-08 09:26:42 -0800

    added a function that retrieve an amenity with Id

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -14,3 +14,12 @@ def get_amenities():
     for amenity in storage.all("Amenity").values():
         amenities.append(amenity.to_dict())
     return jsonify(amenities)
+
+@app_views.route('/amenities/<string:amenity_id>', methods=['GET'],
+                 strict_slashes=False)
+def get_amenity(amenity_id):
+    """get amenity information for specified amenity"""
+    amenity = storage.get("Amenity", amenity_id)
+    if amenity is None:
+        abort(404)
+    return jsonify(amenity.to_dict())
